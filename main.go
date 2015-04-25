f224a88138deb255e8464a8b66e3f66f14eb1302
Dan Sosedoff <dan.sosedoff@gmail.com>
2015-04-24 22:07:40 -0500

    Tweaks

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 	"time"
@@ -11,18 +12,27 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-func main() {
-	if len(os.Args) == 1 {
-		fmt.Println("Usage: ws URL")
-		os.Exit(1)
-	}
+var opts struct {
+	raw bool
+}
 
+func init() {
+	flag.BoolVar(&opts.raw, "raw", false, "Print raw data")
+	flag.Parse()
+}
+
+func main() {
 	dialer := websocket.Dialer{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
 	}
 
-	ws, _, err := dialer.Dial(os.Args[1], nil)
+	if flag.Arg(0) == "" {
+		fmt.Println("Usage: ws URL")
+		os.Exit(1)
+	}
+
+	ws, _, err := dialer.Dial(flag.Arg(0), nil)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(2)
@@ -52,14 +62,22 @@ func main() {
 		}
 
 		var prettyJSON bytes.Buffer
-
 		err = json.Indent(&prettyJSON, mdata, "", "  ")
-		if err == nil {
-			fmt.Printf("%s\n%s\n", ts.Format("20060102-15:04:05.000"), prettyJSON.Bytes())
-		} else {
-			fmt.Printf("%s\n%s\n", ts.Format("20060102-15:04:05.000"), mtype, mdata)
-		}
 
-		fmt.Println("-----------------------------------------------------------")
+		if opts.raw {
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error:", err)
+				continue
+			}
+			fmt.Printf("%s\n", prettyJSON.Bytes())
+		} else {
+			if err == nil {
+				fmt.Printf("%s\n%s\n", ts.Format("20060102-15:04:05.000"), prettyJSON.Bytes())
+			} else {
+				fmt.Printf("%s\n%s\n", ts.Format("20060102-15:04:05.000"), mtype, mdata)
+			}
+
+			fmt.Println("-----------------------------------------------------------")
+		}
 	}
 }
